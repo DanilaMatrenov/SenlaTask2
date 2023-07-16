7d426f1ad2b0dde9bdcd62f78a4682a91132a7b7
Danila Matrenov <djslash1337@gmail.com>
2023-07-16 21:49:22 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿try
+{
+    Console.Write("Введите первое число ");
+    int first = int.Parse(Console.ReadLine());
+    Console.Write("Введите второе число ");
+    int second = int.Parse(Console.ReadLine());
+    int nod, nok;
+    static int nods(int a, int b)
+    {
+        while (a != b)
+        {
+            if (a > b)
+            {
+                a = a - b;
+            }
+            else
+            {
+                b = b - a;
+            }
+
+        }
+        return a;
+    }
+    Console.WriteLine("Нод " + nods(first, second));
+    nok = (first * second) / nods(first, second);
+    Console.WriteLine("Нок " + nok.ToString());
+}
+catch { Console.WriteLine("Ошибка в числах повторите попытку"); }
